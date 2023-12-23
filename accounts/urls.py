51c5e5546928b7215f0d3a6dc1870347ce2bc955
Akshat-Srivastava2004 <akshatsri2004alld@gmail.com>
2023-12-23 19:29:28 +0530

    login_signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,5 +8,6 @@ from . import views
 
 urlpatterns = [
     path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.contrib import auth
 
+def register(request):
+    if request.method == 'POST':
+        userEmail= request.POST.get('email')  
+        password = request.POST.get('password')
+
+
+        new_user = User.objects.create(
+            userEmail=userEmail, 
+        )
+        new_user.set_password(password)
+        new_user.save()
+        subject = 'Welcome to literario website'
+        message = 'Thank you for signing up on our Website. We are excited to have you!'
+        from_email = ''  
+        recipient_list = [new_user.email]
+
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+
+        return redirect('login')
+    return render(request, 'home/index.html')
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method=='POST':
+        email=request.POST.get('email')
+        p_word=request.POST.get('pwd')
+
+        user=auth.authenticate(email=email,password=p_word)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('index')
+        else:
+            return redirect('login')
+    return render(request,'home/index.html') 
